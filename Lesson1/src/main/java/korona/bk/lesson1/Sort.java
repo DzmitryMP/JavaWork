fc47ad5eda504f739c5448d06f0c8d4a3f76f962
Myshkovets <d.myshkovets@kpedev.by>
2025-03-17 15:50:20 +0300

    Задача 2
    Сортировка двухмерного массива

--- Lesson1/src/main/java/korona/bk/lesson1/Sort.java
+++ Lesson1/src/main/java/korona/bk/lesson1/Sort.java
@@ -18,4 +18,34 @@ public class Sort {
         }
     }
 
+    public void sort(char[][] charMas) {
+
+        int rows = charMas.length;
+        int cols = charMas[0].length;
+        int totalElements = rows * cols;
+
+        for (int i = 0; i < totalElements - 1; i++) {
+            for (int j = 0; j < totalElements - 1 - i; j++) {
+                int currentRow = j / cols;
+                int currentCol = j % cols;
+                int nextRow = (j + 1) / cols;
+                int nextCol = (j + 1) % cols;
+
+                if (charMas[currentRow][currentCol] > charMas[nextRow][nextCol]) {
+                    char temp = charMas[currentRow][currentCol];
+                    charMas[currentRow][currentCol] = charMas[nextRow][nextCol];
+                    charMas[nextRow][nextCol] = temp;
+                }
+            }
+        }
+        System.out.println("Вывод масивва");
+        for (char[] row : charMas) {
+            for (char value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
