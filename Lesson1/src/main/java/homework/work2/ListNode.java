e3d7a538004d1f245f6897c6e29031ff9936941e
Myshkovets <d.myshkovets@kpedev.by>
2025-03-27 09:05:22 +0300

    Связный список

---
+++ Lesson1/src/main/java/homework/work2/ListNode.java
@@ -0,0 +1,38 @@
+package homework.work2;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static void print(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) {
+                System.out.print(" -> ");
+            }
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode head1 = new ListNode(1, node2);
+
+        print(head1);
+    }
+}
