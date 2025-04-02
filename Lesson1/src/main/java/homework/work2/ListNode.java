a9bf725e09353780a707bfe1d2e3c71d11fcb4b2
Myshkovets <d.myshkovets@kpedev.by>
2025-04-02 09:40:02 +0300

    Исправление домашки

--- Lesson1/src/main/java/homework/work2/ListNode.java
+++ Lesson1/src/main/java/homework/work2/ListNode.java
@@ -1,8 +1,8 @@
 package homework.work2;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    private int val;
+    private ListNode next;
 
     public ListNode(int val) {
         this.val = val;
@@ -13,7 +13,7 @@ public class ListNode {
         this.next = next;
     }
 
-    public static void print(ListNode head) {
+    public void Print(ListNode head) {
         ListNode current = head;
         while (current != null) {
             System.out.print(current.val);
@@ -25,14 +25,4 @@ public class ListNode {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-
-        ListNode node5 = new ListNode(5);
-        ListNode node4 = new ListNode(4, node5);
-        ListNode node3 = new ListNode(3, node4);
-        ListNode node2 = new ListNode(2, node3);
-        ListNode head1 = new ListNode(1, node2);
-
-        print(head1);
-    }
 }
