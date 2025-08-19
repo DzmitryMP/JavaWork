4b85002da000ebeb8e02b674cca199ae483cc0e1
Myshkovets <d.myshkovets@kpedev.by>
2025-08-19 09:38:12 +0300

    ArrayList Library

--- Lesson1/src/main/java/homework/work2/libary/Library.java
+++ Lesson1/src/main/java/homework/work2/libary/Library.java
@@ -2,13 +2,15 @@ package homework.work2.libary;
 
 import homework.work2.CustomList;
 
+import java.util.ArrayList;
+
 public class Library {
 
 
-    private final CustomList libraryBooks = new CustomList();
+    private final ArrayList libraryBooks = new ArrayList();
 
     public boolean addBook(Book book) {
-        if (!libraryBooks.find(book)) {
+        if (!libraryBooks.contains(book)) {
             libraryBooks.add(book);
             return true;
         } else {
@@ -21,8 +23,8 @@ public class Library {
 
         Book findBooks = new Book(title, author, null);
 
-        for (int i = 0; i < libraryBooks.getSize(); i++) {
-            Book book = (Book) libraryBooks.getObj(i);
+        for (int i = 0; i < libraryBooks.size(); i++) {
+            Book book = (Book) libraryBooks.get(i);
             if (book.equals(findBooks)) {
                 return book;
             }
@@ -31,10 +33,10 @@ public class Library {
     }
 
     // Находит все книги указанного автора
-    public CustomList findBooksByAuthor(Author author) {
-        CustomList books = new CustomList();
-        for (int i = 0; i < libraryBooks.getSize(); i++) {
-            Book book =  (Book)  libraryBooks.getObj(i);
+    public ArrayList findBooksByAuthor(Author author) {
+        ArrayList books = new ArrayList();
+        for (int i = 0; i < libraryBooks.size(); i++) {
+            Book book =  (Book)  libraryBooks.get(i);
             if (book.getAuthor().equals(author)) {
                 books.add(book);
             }
